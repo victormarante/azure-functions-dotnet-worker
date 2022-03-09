e9b9751b3f41f31a3ff81f6fb31104f1526a683f
Shyju Krishnankutty <connectshyju@gmail.com>
2022-03-09 14:44:54 -0800

    something I don't remember

--- src/DotNetWorker.Core/ExtensionStartupRunnner.cs
+++ src/DotNetWorker.Core/ExtensionStartupRunnner.cs
@@ -1,18 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Azure.Functions.Worker.Middleware;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.Functions.Worker.Core
 {
+    internal class ExtensionStartupRunnner
+    {
+        internal static void RunExtensionStartupCode(IFunctionsWorkerApplicationBuilder builder)
+        {
+            var extensionStartupInterfaceType = typeof(IWorkerExtensionStartup);
+
+            WorkerExtensionAssemblyLoader.LoadAssembliesFromDepsJSON();
+
+            var allLoadedAssemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .ToList();
+
+            var extensionAssemblies = allLoadedAssemblies.Where(a => a.GetName().Name.Contains("Worker.Extensions."))
+                .ToList();
+
+            var allTypes = allLoadedAssemblies.SelectMany(assembly => assembly.GetTypes()).ToList();
+
+            var extensionStartupTypes = allTypes
+                .Where(type => !type.IsInterface && extensionStartupInterfaceType.IsAssignableFrom(type))
+                .ToList();
+
+            foreach (var extensionStartupType in extensionStartupTypes)
+            {
+                var instanceAsInterface = Activator.CreateInstance(extensionStartupType) as IWorkerExtensionStartup;
+
+                if (instanceAsInterface != null)
+                {
+                    instanceAsInterface.Configure(builder);
+                }
+            }
+        }
+    }
+
     internal class WorkerExtensionAssemblyLoader
     {
         internal static void LoadAssembliesFromDepsJSON()
@@ -43,31 +68,5 @@ namespace Microsoft.Azure.Functions.Worker.Core
     }
 
 
-    internal class ExtensionStartupRunnner
-    {
-        internal static void RunExtensionStartupCode(IFunctionsWorkerApplicationBuilder builder)
-        {
-            var extensionStartupInterfaceType = typeof(IWorkerExtensionStartup);
 
-            WorkerExtensionAssemblyLoader.LoadAssembliesFromDepsJSON();
-
-            var assemblyList = AppDomain.CurrentDomain.GetAssemblies().ToList();
-
-            var asseblyList2 = assemblyList.Where(a => a.GetName().Name.Contains("Worker.Extensions.")).ToList();
-            var allTypes = assemblyList.SelectMany(assembly => assembly.GetTypes()).ToList();
-            var types = allTypes
-                .Where(type => !type.IsInterface && extensionStartupInterfaceType.IsAssignableFrom(type))
-                .ToList();
-
-            foreach (var type in types)
-            {
-                var instanceAsInterface = Activator.CreateInstance(type) as IWorkerExtensionStartup;
-
-                if (instanceAsInterface != null)
-                {
-                    instanceAsInterface.Configure(builder);
-                }
-            }
-        }
-    }
 }
