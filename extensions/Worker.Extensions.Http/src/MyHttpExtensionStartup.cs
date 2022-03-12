c08234b979c2251c4042ad2c97f12f33f4cc4639
Shyju Krishnankutty <connectshyju@gmail.com>
2022-03-11 21:33:42 -0800

    Consuming http extension package with latest changes

--- extensions/Worker.Extensions.Http/src/MyHttpExtensionStartup.cs
+++ extensions/Worker.Extensions.Http/src/MyHttpExtensionStartup.cs
@@ -6,10 +6,9 @@ namespace Microsoft.Azure.Functions.Worker.Extensions.Http
     public class MyHttpExtensionStartup : IWorkerExtensionStartup
     {
         public void Configure(IFunctionsWorkerApplicationBuilder applicationBuilder)
-        {            
-
+        {
             applicationBuilder.UseMiddleware<StampHttpHeadersMiddleware>();
             applicationBuilder.Services.AddSingleton<IMyFooService, MyFooService>();
-        }
+        }      
     }
 }
